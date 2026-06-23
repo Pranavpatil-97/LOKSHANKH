791096409c6f5c00b54d91324a19b582649d363b
Pranav Patil <pranavpatil92733@gmail.com>
2026-06-23 12:27:14 +0530

    Update CORS settings

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'https://lokshankh.vercel.app/'  
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
